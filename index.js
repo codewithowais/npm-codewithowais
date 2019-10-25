d7cd6e7f59a8d9671661ac700c083a256e629d9b
Muhammad Kamran <smkamranqadri@yahoo.com>
2019-10-25 20:58:57 +0500

    updated the information.

--- index.js
+++ index.js
@@ -3,14 +3,11 @@
 const { white, bold, blueBright } = require('colorette');
 
 const id = 'smkamranqadri';
-const title = 'Technical Lead';
-const company = 'Mikaels Labs';
+const title = 'Nomad Software Engineer (Javascript)';
+const company = 'Looking for Remote Job!';
 const name = 'Kamran Qadri';
 
 const work = white(`${title} @ ${blueBright(company)}`);
-const community = `${blueBright('  Community Manager')} ${white(
-  'at devncode'
-)}`;
 const twitter = blueBright(`https://twitter.com/${id}`);
 const linkedIn = blueBright(`https://linkedin.com/in/${id}`);
 const github = blueBright(`https://github.com/${id}`);
@@ -18,7 +15,6 @@ const npx = white('npx kamranqadri');
 
 const newline = '\n';
 const heading = `${white(`              ${name} /`)} ${blueBright(id)}`;
-const communiting = `${white(bold('  Community:'))}${community}`;
 const working = `${white(bold('       Work:'))}  ${work}`;
 const linkedining = `${white(bold('   LinkedIn:'))}  ${linkedIn}`;
 const twittering = `${white(bold('    Twitter:'))}  ${twitter}`;
