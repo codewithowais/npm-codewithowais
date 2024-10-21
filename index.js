1d26cb1cfedda88d958156930ec057f17bfbf6d2
Muhammad Owais Ahmed <62741318+codewithowais@users.noreply.github.com>
2024-10-22 02:05:32 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
 
 const { white, bold, blueBright } = require('colorette');
 
-const id = 'smkamranqadri';
-const title = 'Nomad Software Engineer (Javascript)';
+const id = 'codewithowais';
+const title = 'Senior Software Engineer (Javascript)';
 const company = 'Looking for Remote Job!';
-const name = 'Kamran Qadri';
+const name = 'Muhammad Owais Ahmed';
 
 const work = white(`${title} @ ${blueBright(company)}`);
 const twitter = blueBright(`https://twitter.com/${id}`);
 const linkedIn = blueBright(`https://linkedin.com/in/${id}`);
 const github = blueBright(`https://github.com/${id}`);
-const npx = white('npx kamranqadri');
+const npx = white('npx codewithowais');
 
 const newline = '\n';
 const heading = `${white(`              ${name} /`)} ${blueBright(id)}`;
